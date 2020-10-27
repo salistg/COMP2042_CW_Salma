bc00e113449b62a59bdcd0dc6c78b3dbaf0a63ce
Salma <62848194+salistg@users.noreply.github.com>
2020-10-27 10:47:42 +0300

    Adding files
    Initial files
    Created p4_group_repo
    Created game.fxml
    Created module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module Frogger {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.media;
+    requires javafx.graphics;
+
+    exports p4_group_8_repo;
+
+    opens p4_group_8_repo;
+
+}
