46cc1fdcfbd4823630f8797366b97595f313f7b5
Salma <62848194+salistg@users.noreply.github.com>
2020-10-27 22:03:18 +0300

    More updates
    - Added title to the frogger application
    - Designed the Start menu (with a start button, an exit button and a help button to help users know how to play the game)
    - Created a background for the Help menu
    - Created a background for the Start menu

--- src/p4_group_8_repo/Start.java
+++ src/p4_group_8_repo/Start.java
@@ -1,24 +1,36 @@
 package p4_group_8_repo;
 
 import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.layout.BackgroundImage;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Start {
     AnimationTimer timer;
     MyStage background;
+    MyStage startScreen;
+    MyStage helpScreen;
+    MyStage exitScreen;
     Animal animal;
     //Stage primaryStage;
 
 
-    public void startGame(Stage primaryStage){
+    public void startGame(Stage primaryStage) throws FileNotFoundException {
         background = new MyStage();
         Scene scene  = new Scene(background,600,800);
+        primaryStage.setTitle("Frogger");
 
 //		Menu menu = new Menu();
 //		background.getChildren().add(menu.createFeatures());
@@ -107,8 +119,72 @@ public class Start {
                 background.playMusic();
         });
 
+
+        helpScreen = new MyStage();
+        Scene helpScene = new Scene(helpScreen,500,600);
+
+        BackgroundImage backgroundImage1 = new BackgroundImage(new Image("file:src/p4_group_8_repo/images/helpBackground.png",500,600, false, true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        helpScreen.setBackground(new Background(backgroundImage1));
+
+
+        Button startButton = new Button("Start game");
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                primaryStage.setScene(scene);
+            }
+        });
+        Button helpButton = new Button("How to play the game");
+        helpButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                primaryStage.setScene(helpScene);
+            }
+        });
+        Button exitButton = new Button("Exit game");
+        exitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.exit(1);
+            }
+        });
+
+        VBox vBox = new VBox();
+        vBox.setPrefWidth(340);
+        vBox.setPrefHeight(50);
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(0,20,10,20));
+        vBox.setAlignment(Pos.CENTER);
+
+        startButton.setMinWidth(vBox.getPrefWidth());
+        startButton.setMinHeight(vBox.getPrefHeight());
+        helpButton.setMinWidth(vBox.getPrefWidth());
+        helpButton.setMinHeight(vBox.getPrefHeight());
+        exitButton.setMinWidth(vBox.getPrefWidth());
+        exitButton.setMinHeight(vBox.getPrefHeight());
+
+        vBox.getChildren().addAll(startButton,helpButton,exitButton);
+        Scene startScene = new Scene(vBox,500,600);
+
+        BackgroundImage backgroundImage2 = new BackgroundImage(new Image("file:src/p4_group_8_repo/images/FroggerBG2.png",500,600, false, true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        vBox.setBackground(new Background(backgroundImage2));
+
+
+        ToggleButton backButton = new ToggleButton("Back");
+        helpScreen.getChildren().add(backButton);
+        backButton.setOnAction(event3 -> {
+            if(backButton.isSelected())
+                primaryStage.setScene(startScene);
+            else
+                primaryStage.setScene(helpScene);
+
+        });
+
+
         background.start();
-        primaryStage.setScene(scene);
+        primaryStage.setScene(startScene);
         primaryStage.show();
         start();
 
