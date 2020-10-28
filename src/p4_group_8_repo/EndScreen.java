fe11c0e9140d387baf79133bd6a4a7bd302800bc
Salma <62848194+salistg@users.noreply.github.com>
2020-10-29 00:25:27 +0300

    Created the high score screen + some extras
    - Created a high score screen that shows when the user finishes the game.
    - High score screen automatically sorts the scores from highest to lowers.
    - Added button in the main screen to view high scores.
    - Created a back button in the high score screen to go back to main menu.
    - Created a back button in the game screen to go back to main menu.

---
+++ src/p4_group_8_repo/EndScreen.java
@@ -0,0 +1,94 @@
+package p4_group_8_repo;
+
+import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
+
+public class EndScreen {
+    public Scene start(Stage stage) throws Exception {
+        Collection<ScoresData> list = Files.readAllLines(new File("scores.txt").toPath()).stream().map(
+                line -> {
+                    String[] details = line.split("\n");
+                    ScoresData scoresData = new ScoresData();
+                    scoresData.setNumber(details[0]);
+                    return scoresData;
+                }
+        ).collect(Collectors.toList());
+
+        ObservableList<ScoresData> details = FXCollections.observableArrayList(list);
+        TableView<ScoresData> tableView = new TableView<>();
+        TableColumn<ScoresData, String> col2 = new TableColumn<>("High Scores");
+        col2.setPrefWidth(250);
+        col2.setStyle("-fx-alignment: CENTER");
+        col2.setCellValueFactory(data -> data.getValue().scoreProperty());
+
+        tableView.getColumns().addAll(col2);
+
+
+        Callback<ScoresData,Observable[]> callback = (ScoresData scoresData) -> new Observable[]{
+                scoresData.scoreProperty(),
+        };
+
+        ObservableList<ScoresData> observableList = FXCollections.observableArrayList(callback);
+        observableList.addAll(details);
+
+        SortedList<ScoresData> sortedList = new SortedList<ScoresData>(observableList,
+                (ScoresData scores1, ScoresData scores2) ->{
+            if(Integer.valueOf(scores1.getScores()) > Integer.valueOf(scores2.getScores()))
+                return -1;
+            else if(Integer.valueOf(scores1.getScores()) < Integer.valueOf(scores2.getScores()))
+                return 1;
+            else
+                return 0;
+                });
+
+
+        tableView.setItems(sortedList);
+
+        Start start = new Start();
+        VBox vBox = new VBox(tableView);
+        vBox.setAlignment(Pos.CENTER);
+        ToggleButton backButton = new ToggleButton("Back");
+        vBox.getChildren().add(backButton);
+        backButton.setOnAction(event3 -> {
+            if(backButton.isSelected()) {
+                try {
+                    start.startGame(stage);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        Scene endScene = new Scene(vBox, 250, 390);
+
+        return endScene;
+
+    }
+
+
+}
+
